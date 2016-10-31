a5661e51b48e83997655e5d8d3fd38e078988ea0
fireremnant <ry.schrader@gmail.com>
2016-10-31 18:47:09 +1100

    Load menu

--- unit_05/js/script.js
+++ unit_05/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	
 	// load nav with callback
-	$("#nav").load("sections/nav.html", function() {
+	$("#main-menu").load("sections/nav.html", function() {
 		// alert( "Load was performed." );
 	});
 
