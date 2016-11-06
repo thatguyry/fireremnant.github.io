e75d4fc98d27a47769b0ff8e3832a0498cf30f06
fireremnant <ry.schrader@gmail.com>
2016-11-06 15:42:42 +1100

    Disable Page

--- final_project/js/script.js
+++ final_project/js/script.js
@@ -1,67 +1,108 @@
-$(document).ready(function() {
-	
-	// START LOAD SECTIONS
+// START LOAD SECTIONS
 
-	// load nav with callback
-	$( "#main-menu" ).load( "sections/nav.html" , function() {
-		console.log("main menu was loaded successfully.");
+// load nav with callback
+$( "#main-menu" ).load( "sections/nav.html" , function() {
+	console.log("main menu was loaded successfully.");
+});
+
+// load footer with callback
+$( "#footer" ).load( "sections/footer.html" , function() {
+	console.log("footer was loaded successfully.");
+});
+
+// END LOAD SECTIONS
+
+// START STICKY
+
+// declare variables
+var header = document.querySelector('#header');
+var headerPosition = header.getBoundingClientRect();
+var placeholder = document.createElement('div');
+
+// setup
+placeholder.style.width = headerPosition.width + 'px';
+placeholder.style.height = headerPosition.height + 'px';
+var isAdded = false;
+
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset >= headerPosition.top && !isAdded) {
+        header.classList.add('sticky');
+        header.parentNode.insertBefore(placeholder, header);
+        isAdded = true;
+    } else if (window.pageYOffset == headerPosition.top && isAdded) {
+        console.log(isAdded);
+        header.classList.remove('sticky');
+        header.parentNode.removeChild(placeholder);
+        isAdded = false;
+    }
+});	
+
+// END STICKY
+
+// START TYPED
+
+$(function(){
+  $("#status").typed({
+    strings: [
+    	"I'm counting pixels." ,
+    	"I'm guessing breakpoints." ,
+    	"I'm listening to Spotify."],
+    
+    // typing speed
+    typeSpeed: 0,
+	// time before backspacing
+    backDelay: 3000,
+    // loop
+    loop: false,
+    // show cursor
+    showCursor: false,
+
+  });
+});
+
+// END TYPED
+
+// START DISABLE PAGE
+
+var page = document.querySelector('.page');
+
+// variables
+var pageDisabled = false;
+
+// function - disable page
+function disablePage() {
+
+	$(page).addClass("disabled");
+	pageDisabled = true;
+	// reset for testing
+	setTimeout(enablePage, 3000);
+
+};
+
+// function - enable page
+function enablePage() {
+
+	$(page).removeClass("disabled");
+	pageDisabled = false;
+
+};
+
+// END DISABLE PAGE
+
+// START INTERACTIONS
+
+var ctaWork = document.querySelector('#cta-work');
+var thumb = document.querySelector('.thumb');
+
+$(ctaWork).click(function() {
+	disablePage();
+});
+
+$(thumb).click(function() {
+	disablePage();
+	$('#page-container').load( "test.html" , function() {
+		console.log("Test was loaded successfully.");
+		enablePage();
+		$('html, body').animate({ scrollTop: 0 }, 0);
 	});
-
-	// load footer with callback
-	$( "#footer" ).load( "sections/footer.html" , function() {
-		console.log("footer was loaded successfully.");
-	});
-
-	// END LOAD SECTIONS
-
-	// START STICKY
-
-	// declare variables
-	var menu = document.querySelector('#nav');
-	var menuPosition = menu.getBoundingClientRect();
-	var placeholder = document.createElement('div');
-	
-	// setup
-	placeholder.style.width = menuPosition.width + 'px';
-	placeholder.style.height = menuPosition.height + 'px';
-	var isAdded = false;
-
-	window.addEventListener('scroll', function() {
-	    if (window.pageYOffset >= menuPosition.top && !isAdded) {
-	        menu.classList.add('sticky');
-	        menu.parentNode.insertBefore(placeholder, menu);
-	        isAdded = true;
-	    } else if (window.pageYOffset == menuPosition.top && isAdded) {
-	        console.log(isAdded);
-	        menu.classList.remove('sticky');
-	        menu.parentNode.removeChild(placeholder);
-	        isAdded = false;
-	    }
-	});	
-
-	// END STICKY
-
-	// START TYPED
-
-	$(function(){
-	  $("#status").typed({
-	    strings: [
-	    	"I'm counting pixels." ,
-	    	"I'm guessing breakpoints." ,
-	    	"I'm listening to Spotify."],
-	    
-	    // typing speed
-	    typeSpeed: 0,
-		// time before backspacing
-        backDelay: 3000,
-        // loop
-        loop: false,
-        // show cursor
-        showCursor: false,
-
-	  });
-	});
-
-	// END TYPED
-
 });
