388720a8c4fc1ac4cf4dbeb73f11ec6e6ce095ae
fireremnant <ry.schrader@gmail.com>
2016-11-09 07:31:33 +1100

    url handling - part 1

--- final_project/js/script.js
+++ final_project/js/script.js
@@ -95,7 +95,7 @@ var ctaWork = document.querySelector('#cta-work');
 var thumb = document.querySelector('.thumb');
 
 $(ctaWork).click(function() {
-	disablePage();
+	// disablePage();
 });
 
 $(thumb).click(function() {
@@ -106,3 +106,68 @@ $(thumb).click(function() {
 		$('html, body').animate({ scrollTop: 0 }, 0);
 	});
 });
+
+// URL HANDLING
+
+// define variables
+var container = document.querySelector('#main-menu');
+var imgs = document.querySelectorAll('#main-menu a');
+var textWrapper = document.querySelector('.highlight');
+var content = document.querySelector('#page-container');
+var defaultTitle = "Select your Ghostbuster!";
+var fileType = ".html";
+
+function updateText(content){
+    textWrapper.innerHTML = content;
+}
+
+function requestContent(file){
+    $(content).load(file);
+}
+
+function removeCurrentClass(){
+    console.log('removeCurrentClass');
+    for(var i = 0; i < imgs.length; i++){
+        console.log('imgs ' + imgs[i]);
+        imgs[i].classList.remove('current');
+    }
+}
+
+function addCurrentClass(elem){
+    removeCurrentClass();
+    var element = document.querySelector("." + elem);
+    element.classList.add('current');
+}
+
+container.addEventListener('click', function(e){
+    if(e.target != e.currentTarget){
+        e.preventDefault();
+        var data = e.target.getAttribute('data-name'),
+        url = data + fileType;
+        console.log(data);
+        addCurrentClass(data);
+        history.pushState(data, null, url);
+        updateText(data);
+        requestContent(url);
+        document.title = "Ghostbuster | " + data;
+    }
+e.stopPropagation();
+}, false);
+
+window.addEventListener('popstate', function(e){
+    console.log('character' + character);
+    var character = e.state;
+
+    if (character == null) {
+        removeCurrentClass();
+        textWrapper.innerHTML = " ";
+        content.innerHTML = " ";
+        document.title = defaultTitle;
+    } else {
+        updateText(character);
+        requestContent(character + fileType);
+        addCurrentClass(character);
+        document.title = "Ghostbuster | " + character;
+    }
+
+})
